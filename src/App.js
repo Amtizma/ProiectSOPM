dbf1e9c8602133812871f0e967d4261e27b16ac1
Amtizma <denesflorin37@gmail.com>
2023-11-22 14:47:47 +0200

    Combined the two codes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CategoryPage from './Category';
+import './categories.css';
 
 const TopBar = () => {
-  return (
-    <div className="top-bar">
-      <div className="buttons">
-        <button className="button">Automation</button>
-        <button className="button">Filters</button>
-        <button className="button">Share</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="top-bar">
+            <div className="buttons">
+                <button className="button">Automation</button>
+                <button className="button">Filters</button>
+                <button className="button">Share</button>
+            </div>
+            <CategoryPage />
+        </div>
+    );
 };
-
-
 
 const App = () => {
-  return (
-    <div className="App">
-      {/* Restul codului tău existent */}
-      <div className="categorii">
-        <div className="categorie">
-          <h2>Todo</h2>
-          {/* Adaugă aici lista cu activități todo */}
+    return (
+        <div className="App">
+            {/* Restul codului tău existent */}
+            <TopBar /> {/* Adaugă aici componenta TopBar */}
+            <div className="categorii">
+                <div className="categorie">
+                    <h2>Todo</h2>
+                    {/* Adaugă aici lista cu activități todo */}
+                </div>
+                <div className="categorie">
+                    <h2>Ongoing</h2>
+                    {/* Adaugă aici lista cu activități în desfășurare */}
+                </div>
+                <div className="categorie">
+                    <h2>Done</h2>
+                    {/* Adaugă aici lista cu activități realizate */}
+                </div>
+            </div>
         </div>
-        <div className="categorie">
-          <h2>Ongoing</h2>
-          {/* Adaugă aici lista cu activități în desfășurare */}
-        </div>
-        <div className="categorie">
-          <h2>Done</h2>
-          {/* Adaugă aici lista cu activități realizate */}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-
 export default App;
-
