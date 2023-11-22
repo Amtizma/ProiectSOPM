f5d84aeafbf260b5d16904052df7b448127188f4
Amtizma <denesflorin37@gmail.com>
2023-11-22 16:41:18 +0200

    united

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import './topbar.css'; // Import the TopBar CSS file
-import TopBar from './topbar'; // Import the TopBar component
+import logo from './logo.svg';
+import './App.css';
+import CategoryPage from './Category';
+import './categories.css';
+import './topbar.css';
+import TopBar from './topbar';
 
 const App = () => {
-  return (
-    <div className="App">
-      {/* Other content in your app */}
-      <TopBar /> {/* Include the TopBar component */}
-      {/* Other content in your app */}
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* Restul codului tău existent */}
+            <TopBar /> {/* Adaugă aici componenta TopBar */}
+            <div className="categoriesAndTasks">
+            <CategoryPage />
+            <div className="categorii">
+                <div className="categorie">
+                    <h2>Todo</h2>
+                    {/* Adaugă aici lista cu activități todo */}
+                </div>
+                <div className="categorie">
+                    <h2>Ongoing</h2>
+                    {/* Adaugă aici lista cu activități în desfășurare */}
+                </div>
+                <div className="categorie">
+                    <h2>Done</h2>
+                    {/* Adaugă aici lista cu activități realizate */}
+                </div>
+            </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default App;
