9b3f295fee4fbe438766854495892b89e69ba708
Diana <diana.dutuu@gmail.com>
2023-11-22 10:48:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const TopBar = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="top-bar">
+      <div className="buttons">
+        <button className="button">Automation</button>
+        <button className="button">Filters</button>
+        <button className="button">Share</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TopBar;
