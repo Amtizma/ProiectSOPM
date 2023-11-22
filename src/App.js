1622b6cd7bf2736294edfe964a6aeaabe1ca6ee1
Amtizma <denesflorin37@gmail.com>
2023-11-22 17:06:35 +0200

    ab

--- src/App.js
+++ src/App.js
@@ -9,22 +9,18 @@ import TopBar from './topbar';
 const App = () => {
     return (
         <div className="App">
-            {/* Restul codului tău existent */}
-            <TopBar /> {/* Adaugă aici componenta TopBar */}
+            <TopBar />
             <div className="categoriesAndTasks">
             <CategoryPage />
             <div className="categorii">
                 <div className="categorie">
                     <h2>Todo</h2>
-                    {/* Adaugă aici lista cu activități todo */}
                 </div>
                 <div className="categorie">
                     <h2>Ongoing</h2>
-                    {/* Adaugă aici lista cu activități în desfășurare */}
                 </div>
                 <div className="categorie">
                     <h2>Done</h2>
-                    {/* Adaugă aici lista cu activități realizate */}
                 </div>
             </div>
             </div>
