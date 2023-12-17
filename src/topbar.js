8d98750444fce2e7997621fe15aafa6fca48b3bc
Diana <diana.dutuu@gmail.com>
2023-12-17 15:58:08 +0200

    topbar mobile friendly

--- src/topbar.js
+++ src/topbar.js
@@ -2,35 +2,25 @@ import './topbar.css';
 import 'boxicons/css/boxicons.min.css';
 import logo from './images/logo.png';
 
-
-
-
 const TopBar = () => {
   return (
-<div className="top-bar">
-
-<div className="left-section">
-
-<img src={logo} alt='To Do' className='logo'  />
-
-<h1 className="charter-title">Charter for</h1>
-</div>
-
-<div className="right-section">
-<div className="buttons">
-<button className="button">Automation</button>
-<button className="button">Filters</button>
-<button className="button">Share</button>
-
-<button className="button settings-button">
-<i className='bx bx-dots-horizontal-rounded'></i> 
-</button>
-
-</div>
-</div>
-</div>
+    <div className="top-bar">
+      <div className="left-section">
+        <img src={logo} alt='To Do' className='logo' />
+        <h1 className="charter-title">Charter for</h1>
+      </div>
+      <div className="right-section">
+        <div className="buttons">
+          <button className="button">Automation</button>
+          <button className="button">Filters</button>
+          <button className="button">Share</button>
+          <button className="button settings-button">
+            <i className='bx bx-dots-horizontal-rounded'></i>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
-
 
 export default TopBar;
