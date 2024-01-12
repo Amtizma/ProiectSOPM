bbb07dafc41ae0531e0921669a2d07d54384c887
Alina <alina.dumitrica22@gmail.com>
2024-01-12 16:59:06 +0200

    fixed 1st category color asig bug

--- src/CategoryDef.js
+++ src/CategoryDef.js
@@ -1,31 +1,28 @@
 import React from 'react';
 
-var _pastelColors = []
+const _pastelColors = ['#FFDDC1', '#B5EAD7', '#FFC3A0', '#B8F2E6', '#FFAAA7', '#D8E2DC', '#F4A261', '#2A9D8F', '#E9C46A', '#264653'];
 
-const Categories = ({ categories, onDelete}) => {
-    const pastelColors = ['#FFDDC1', '#B5EAD7', '#FFC3A0', '#B8F2E6', '#FFAAA7', '#D8E2DC', '#F4A261', '#2A9D8F', '#E9C46A', '#264653'];
-
-    _pastelColors = pastelColors
-
-    var assignBackgroundColor = (index) => {
-        return {
-            backgroundColor: pastelColors[index % pastelColors.length]
-        };
-    };
-    return (
-        <div>
-            <h2>Categories</h2>
-            <ul>
-                {categories.map((category, index) => (
-                    <li key={index} style={assignBackgroundColor(index)}>{category}
-                        <button onClick={() => onDelete(index)} style={assignBackgroundColor(index)} className='delete-category-button'>x</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+const assignBackgroundColor = (index) => {
+  return {
+    backgroundColor: _pastelColors[index % _pastelColors.length]
+  };
 };
 
-export var assignBackgroundColor;
-export var _pastelColors;
+const Categories = ({ categories, onDelete }) => {
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        {categories.map((category, index) => (
+          <li key={index} style={assignBackgroundColor(index)}>
+            {category}
+            <button onClick={() => onDelete(index)} style={assignBackgroundColor(index)} className='delete-category-button'>x</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export { assignBackgroundColor, _pastelColors };
 export default Categories;
